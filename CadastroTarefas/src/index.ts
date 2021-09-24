dbbd43b044ac4c287c0e8f4c5ce5ced5e8703b9a
Giuliana Costacurta <giulianacostacurta@outlook.com>
2021-09-24 15:20:56 -0300

    Primeiro commit

--- CadastroTarefas/src/index.ts
+++ CadastroTarefas/src/index.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import routes from "./routes";
+import routes from "./routes"; 
 
 const app = express()
 createConnection()
